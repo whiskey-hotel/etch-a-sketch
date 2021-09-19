d5ff8a6fa346cfc053ca3569980bd0e301ef280d
Justin Thornton <jathom15@gmail.com>
2021-09-19 01:43:29 -0500

    Hover features and page format

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,14 +1,34 @@
-const container = document.querySelector("#container");
-
-const dims = 16;
+const container = document.querySelector('#container');
+const dims = 8;
 
 container.style.gridTemplateColumns = `repeat(${dims}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${dims}, 1fr)`;
 
 for (let i = 0; i < Math.pow(dims, 2); i++) {
-	const divs = document.createElement("div");
+	const divs = document.createElement('div');
 	divs.id = `div${i}`;
-    divs.classList.add("highlight");
-	divs.textContent = `${i}`;
+	divs.classList.add('highlight');
+	// divs.textContent = `${i}`;
+	divs.style.backgroundColor = 'rgb(255, 255, 255)';
 	container.appendChild(divs);
+
+	divs.onmouseover = function () {
+		switch (divs.style.backgroundColor) {
+			case 'rgb(255, 255, 255)':
+				divs.style.backgroundColor = 'rgb(204, 204, 204)';
+				break;
+			case 'rgb(204, 204, 204)':
+				divs.style.backgroundColor = 'rgb(153, 153, 153)';
+				break;
+			case 'rgb(153, 153, 153)':
+				divs.style.backgroundColor = 'rgb(102 ,102 ,102)';
+				break;
+			case 'rgb(102, 102, 102)':
+				divs.style.backgroundColor = 'rgb(51, 51, 51)';
+				break;
+			case 'rgb(51, 51, 51)':
+				divs.style.backgroundColor = 'rgb(0, 0, 0)';
+				break;
+		}
+	};
 }
