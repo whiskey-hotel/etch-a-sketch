fac32810013d2a81d64b038a128ad77d33504388
Justin Thornton <jathom15@gmail.com>
2021-09-23 23:45:11 -0500

    Refactoring logic.js

--- scripts/logic.js
+++ scripts/logic.js
@@ -2,44 +2,70 @@ const container = document.querySelector('#container');
 const reset = document.querySelector('#reset');
 const dimensionSelector = document.querySelector('#dims');
 let dims = 8;
-dimensionSelector.addEventListener('change', function () {
-	dims = this.value;
-	container.style.gridTemplateColumns = `repeat(${dims}, 1fr)`;
-	container.style.gridTemplateRows = `repeat(${dims}, 1fr)`;
-	console.log(this.value);
-});
-
 container.style.gridTemplateColumns = `repeat(${dims}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${dims}, 1fr)`;
 
-for (let i = 0; i < Math.pow(dims, 2); i++) {
-	const divs = document.createElement('div');
-	divs.id = `div${i}`;
-	divs.classList.add('highlight');
-	// divs.textContent = `${i}`;
-	divs.style.backgroundColor = 'rgb(255, 255, 255)';
-	container.appendChild(divs);
-
-	divs.onmouseover = function () {
-		switch (divs.style.backgroundColor) {
-			case 'rgb(255, 255, 255)':
-				divs.style.backgroundColor = 'rgb(204, 204, 204)';
-				break;
-			case 'rgb(204, 204, 204)':
-				divs.style.backgroundColor = 'rgb(153, 153, 153)';
-				break;
-			case 'rgb(153, 153, 153)':
-				divs.style.backgroundColor = 'rgb(102 ,102 ,102)';
-				break;
-			case 'rgb(102, 102, 102)':
-				divs.style.backgroundColor = 'rgb(51, 51, 51)';
-				break;
-			case 'rgb(51, 51, 51)':
-				divs.style.backgroundColor = 'rgb(0, 0, 0)';
-				break;
-		}
-	};
-	reset.addEventListener('click', function () {
-		divs.style.backgroundColor = 'rgb(255, 255, 255)';
-	});
+function setDimensions() {
+	dims = dimensionSelector.value;
+	container.style.gridTemplateColumns = `repeat(${dims}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${dims}, 1fr)`;
 }
+
+function createDivs(dims) {
+	for (let i = 0; i < Math.pow(dims, 2); i++) {
+		const tempDivs = document.createElement('div');
+		tempDivs.id = `div${i}`;
+		tempDivs.classList.add('highlight');
+		tempDivs.addEventListener('mouseover', function () {
+			blackAndWhiteDivs(tempDivs);
+		});
+		tempDivs.style.backgroundColor = 'rgb(255, 255, 255)';
+		container.appendChild(tempDivs);
+	}
+}
+
+function blackAndWhiteDivs(divs1) {
+	switch (divs1.style.backgroundColor) {
+		case 'rgb(255, 255, 255)':
+			divs1.style.backgroundColor = 'rgb(204, 204, 204)';
+			break;
+		case 'rgb(204, 204, 204)':
+			divs1.style.backgroundColor = 'rgb(153, 153, 153)';
+			break;
+		case 'rgb(153, 153, 153)':
+			divs1.style.backgroundColor = 'rgb(102 ,102 ,102)';
+			break;
+		case 'rgb(102, 102, 102)':
+			divs1.style.backgroundColor = 'rgb(51, 51, 51)';
+			break;
+		case 'rgb(51, 51, 51)':
+			divs1.style.backgroundColor = 'rgb(0, 0, 0)';
+			break;
+	}
+}
+
+function resetDivs(divs) {
+	for (let d of divs) {
+		d.style.backgroundColor = 'rgb(255, 255, 255)';
+	}
+}
+
+//
+
+createDivs(dims);
+
+divs = document.getElementsByClassName('highlight');
+
+reset.addEventListener('click', function () {
+	resetDivs(Array.from(divs));
+});
+
+dimensionSelector.addEventListener('change', function () {
+	while (divs.length.length > 0) {
+		let d = divs[0];
+		d.container.removeChild(d);
+	}
+	resetDivs(Array.from(divs));
+	setDimensions();
+	createDivs(dims);
+});
